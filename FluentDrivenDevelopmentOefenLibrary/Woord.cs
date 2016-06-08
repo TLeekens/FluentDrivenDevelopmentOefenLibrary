555aea53c9299a3f540e7db186b29b84d0ff2482
TLeekens <tony.leekens@skynet.be>
2016-06-08 09:52:07 +0200

    Woordtest gebouwd met Fluent Assertions, alle tests falen, klaar om Woord te implementeren

---
+++ FluentDrivenDevelopmentOefenLibrary/Woord.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentDrivenDevelopmentOefenLibrary
+{
+    public class Woord
+    {
+        public Woord(string woord)
+        {
+            throw new NotImplementedException();
+        }
+        public bool IsPalindroom()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FluentDrivenDevelopmentOefenLibraryTest/WoordTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using FluentDrivenDevelopmentOefenLibrary;
+
+namespace FluentDrivenDevelopmentOefenLibraryTest
+{
+    [TestClass]
+    public class WoordTest
+    {
+        private Woord woord;
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void ConstructorAansprekenMetLegeStringWerptArgumentException()
+        {
+            Action act = () => new Woord(string.Empty);
+            act.ShouldThrow<ArgumentException>("empty strings are Schroedinger's palindromes");
+        }
+
+        [TestMethod]
+        public void IsPalindroomOpEenWoordMetOnevenAantalLettersDatEchtEenPalindroomIsGeeftTrue()
+        {
+            woord = new Woord("lepel");
+            woord.IsPalindroom().Should().BeTrue("spoons are palindromes");
+        }
+
+        [TestMethod]
+        public void IsPalindroomOpEenWoordMetOnevenAantalLettersDatGeenPalindroomIsGeeftFalse()
+        {
+            woord = new Woord("mes");
+            woord.IsPalindroom().Should().BeFalse("knives backwards are sevink");
+        }
+
+        [TestMethod]
+        public void IsPalindroomOpEenWoordMetEvenAantalLettersDatEchtEenPalindroomIsGeeftTrue()
+        {
+            woord = new Woord("parterretrap");
+            woord.IsPalindroom().Should().BeTrue("going up takes as many steps as going down");
+        }
+
+        [TestMethod]
+        public void IsPalindroomOpEenWoordMetEvenAantalLettersDatGeenPalindroomIsGeeftFalse()
+        {
+            woord = new Woord("vork");
+            woord.IsPalindroom().Should().BeFalse("forks are not always tridents");
+        }
+    }
+}
