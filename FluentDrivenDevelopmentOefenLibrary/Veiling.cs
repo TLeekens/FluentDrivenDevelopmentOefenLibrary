068e132fea00fb20ee6c1c1b5c9a02c819b17e21
TLeekens <tony.leekens@skynet.be>
2016-06-08 10:58:00 +0200

    VeilingTest klaar met FluentAssertions, gereed om Veiling te implementeren

---
+++ FluentDrivenDevelopmentOefenLibrary/Veiling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentDrivenDevelopmentOefenLibrary
+{
+    public class Veiling
+    {
+        public decimal HoogsteBod
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void DoeBod(decimal bedrag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FluentDrivenDevelopmentOefenLibraryTest/VeilingTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using FluentDrivenDevelopmentOefenLibrary;
+
+namespace FluentDrivenDevelopmentOefenLibraryTest
+{
+    [TestClass]
+    public class VeilingTest
+    {
+        private Veiling veiling;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            veiling = new Veiling();
+        }
+
+        [TestMethod]
+        public void AlsGeenEnkelBodIngevoerdIsIsHetHoogsteBodGelijkAanNul()
+        {
+            veiling.HoogsteBod.ShouldBeEquivalentTo(decimal.Zero, "none is none");
+        }
+
+        [TestMethod]
+        public void NaInvoerenVanExactEenBodIsHetHoogsteBodGelijkAanHetBedragVanDatBod()
+        {
+            decimal bedrag = 1000.0m;
+            veiling.DoeBod(bedrag);
+            veiling.HoogsteBod.Should().Be(bedrag);
+        }
+
+        [TestMethod]
+        public void NaInvoerenVanMeerdereBodenIsHetHoogsteBodGelijkAanHetHoogsteBedrag()
+        {
+            decimal hoogsteBedrag = 2000.0m;
+            veiling.DoeBod(1000.0m);
+            veiling.DoeBod(hoogsteBedrag);
+            veiling.DoeBod(1500.0m);
+            veiling.HoogsteBod.Should().Be(hoogsteBedrag);
+        }
+    }
+}
